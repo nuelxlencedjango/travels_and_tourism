c6134e2c3849e5d8e9172bff1dff30c0c5364482
nuelxlencedjango <nuelxlence@gmail.com>
2024-05-10 19:18:00 +0100

    carousel adjutment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 
 import React, { useEffect } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js"
+
 
 
 
@@ -10,6 +12,7 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navs from "./Components/Navs";
 
--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -8,7 +8,7 @@ import Products from './Products';
 import PopularDestinations from './PopularDestinations';
 
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../../../node_modules/bootstrap/dist/js/bootstrap.bundle.js"
+import "../../../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
 
 
 
